786a86ee19ae078a0604e69de47e560913948175
Ryna Truong <rynatruong@outlook.com>
2020-06-22 16:48:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
 def my_each(array)
   i = 0
-  while i < array.length 
+  while i < array.length
     yield(array[i])
-      i = i + 1
-    end
-    array
+    i = i + 1
   end
+ 
+  array
 end
 
   
